a9083b963e8746f247acc71b317fb61bcd699cc0
Ricky Johnson <rickyjohnson2018@gmail.com>
2024-01-18 17:50:00 -0500

    Finished Lab 0

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,20 @@
-//HelloWorld.java
+// Programmers: Ricky Johnson
+// Course: CS 212, Mr. John
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: n/a
+
+// HelloWorld.java
 
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println(" __       ___");
+        System.out.println("|__|       |");
+        System.out.println("|  \\       |");
+        System.out.println("|   \\  \\___/");
     }
 }
